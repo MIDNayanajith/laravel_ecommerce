92cdfbf41bac88693fd707d1fad2a0d598006e9e
MIDNayanajith <isuranga_2020736@fot.ruh.ac.lk>
2025-06-30 22:39:04 +0530

    develop admin category and brand function

---
+++ ecommerce/app/Models/Order.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+        // Allow mass assignment for these fields
+    protected $fillable = [
+        'user_id',
+        'grand_total',
+        'payment_method',
+        'payment_status',
+        'status',
+        'currency',
+        'shipping_amount',
+        'shipping_method',
+        'notes',
+    ];
+
+    // Define the relationship with the User model
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Define the relationship with the OrderItem model
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    // Define the relationship with the ShippingAddress model
+    public function address()
+    {
+        return $this->belongsTo(Address::class);
+    }
+}
